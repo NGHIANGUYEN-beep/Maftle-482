4ed6064e37d55e58068d15032c9c04aeff3a538e
Aaron Nguyen <nghianguyen@compsci.adelphi.edu>
2025-09-04 15:46:21 -0400

    Account commit

---
+++ Account.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+maftleAcc = SQLAlchemy()
+class Accounts(maftleAcc.Model):
+    maftleAcc = "users"
+    id = maftleAcc.Column(maftleAcc.Integer, primary_key = True)
+    username = maftleAcc.Column(maftleAcc.String(100), nullable=False)
+    email = maftleAcc.Column(maftleAcc.String(120), unique=True, nullable=False)
+    password = maftleAcc.Column(maftleAcc.String(100), nullable = False)
+    
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        super().__init__()
