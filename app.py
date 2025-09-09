a89de251bc5d493fe5eb9b7ce157cd01f11b99a7
Aaron Nguyen <nghianguyen@compsci.adelphi.edu>
2025-09-09 17:15:46 -0400

    something

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from Account import maftleAcc
-maftleAcc = SQLAlchemy()
 
 def accountImport():
 
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://nghianguyen@compsci.adelphi.edu:epicexplosion1@localhost/fall2025_482m"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     maftleAcc.init_app(app)
     
     from accRoute import bp
