646ed22a6c50e7066ab7ac4578927504c8838d24
Aaron Nguyen <nghianguyen@compsci.adelphi.edu>
2025-09-09 13:25:38 -0400

    changed some stuff

--- accRoute.py
+++ accRoute.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session, Flask
 from Account import maftleAcc, Accounts
 
+#BP for running the home page, only uses the template
 bp = Blueprint('main', __name__)
 @bp.route("/")
 def home():
     return render_template("homepage.html")
 
+#BP for creating an account
 @bp.route("/create", methods = ["GET", "POST"])
 def create_account():
+    
     if request.method == "POST":
         username = request.form.get("username")
         email = request.form.get("email")
@@ -20,3 +23,31 @@ def create_account():
         return redirect('/')
     
     return render_template("createaccount.html")
+
+#BP for logging in
+@bp.route("/login", methods = ["GET", "POST"])
+def login():
+    
+    #Obtaining Email and Password
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        #Filters through accounts with the same email
+        user = Accounts.query.filter_by(email=email).first()
+        
+        #Finds account
+        if user and user.password == password:
+            session[user.id] = user.id
+            session[password] = user.password
+            flash("Login Successful!", "Success")
+            
+        #Redirects if valid
+            return redirect('/')
+        
+        else:
+        #Redirects if invalid
+            flash("Invalid credentials", "Error")
+            return redirect("/login")
+        
+    return render_template("loginpage.html")
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+maftleAcc = SQLAlchemy()
+
+def accountImport():
+
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://nghianguyen@compsci.adelphi.edu:epicexplosion1@localhost/fall2025_482m"
+    maftleAcc.init_app(app)
+    
+    from accRoute import bp
+    app.register_blueprint(bp)
+    
+    
+    
+    
